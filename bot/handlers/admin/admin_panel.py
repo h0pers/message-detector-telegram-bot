6845b9d6ef9102a98004d37e9a860599e153dfb8
Dmytro Hryshchenko <zzzxxxxx28540@gmail.com>
2024-05-08 01:26:51 +0100

    Admin panel text

--- bot/handlers/admin/admin_panel.py
+++ bot/handlers/admin/admin_panel.py
@@ -2,6 +2,7 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command, StateFilter
 
+from bot.config import MessageText
 from bot.keyboards.inline.admin.admin_panel import admin_panel_inline_markup
 
 admin_panel_router = Router()
@@ -9,4 +10,5 @@ admin_panel_router = Router()
 
 @admin_panel_router.message(StateFilter(None), Command(commands=['admin', 'start']))
 async def admin_panel_handler(message: Message):
-    await message.answer(text='admin panel', reply_markup=admin_panel_inline_markup.get_markup())
+    await message.answer(text=MessageText.ADMIN_PANEL.format(username=message.from_user.username or message.from_user.first_name),
+                         reply_markup=admin_panel_inline_markup.get_markup())
